a92448814099975678a23216367cf49787eda6d2
AWS-123 <124189211+AWS-123@users.noreply.github.com>
2024-07-09 23:31:33 +0530

    Update App.java

--- sample-maven-pro/src/main/java/com/phonepay/App.java
+++ sample-maven-pro/src/main/java/com/phonepay/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
           System.out.println( "this maven build!" );
+        System.out.println( "this maven trigger!" );
     }
 }
