e5bb169f6efdb2fd604127115df0479f5fc07860
root <root@ip-172-31-10-223.us-west-1.compute.internal>
2024-07-11 16:55:16 +0000

    new-update

---
+++ src/main/java/com/phonepay/App.java
@@ -0,0 +1,15 @@
+package com.phonepay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+          System.out.println( "this maven build!" );
+        System.out.println( "this maven trigger!,hello trigger please respond,hello" );
+    }
+}
